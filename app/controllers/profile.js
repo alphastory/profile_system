25458dfd3993eb34295210e306350c939581bc22
Faculty <Faculty@AI391LAB0213MIN.local>
2016-09-13 20:41:23 -0400

    finish crud operations on profiles

--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	isEditing: false,
+
 	age: Ember.computed( 'model.dob', function(){
 		let dob = this.get( 'model.dob' );
 		let dateObject = new Date( dob );
@@ -8,5 +10,27 @@ export default Ember.Controller.extend({
 		let ageDate = new Date( diff );
 
 		return Math.abs( ageDate.getUTCFullYear() - 1970 );
-	} )
+	} ),
+
+	actions: {
+		deleteProfile(){
+			let profile = this.get( 'model' );
+			console.log( 'Deleting ' + profile.get( 'name' ) );
+			profile.deleteRecord();
+			profile.save();
+
+			this.transitionToRoute( 'profiles' );
+		},
+
+		editProfile(){
+			this.set( 'isEditing', true );
+		},
+
+		saveChanges(){
+			let profile = this.get( 'model' );
+			profile.save();
+			
+			this.set( 'isEditing', false );
+		}
+	}
 });
